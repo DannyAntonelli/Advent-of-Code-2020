d5cebb609e2b032a197ebd17a05c49001a2a9576
DannyAntonelli <dannyantonelli2002@gmail.com>
2020-12-13 12:41:41 +0100

    Day 13 completed part 2

--- Day13/day13.py
+++ Day13/day13.py
@@ -1,4 +1,4 @@
-with open("test.txt", "r", encoding="UTF-8") as fin:
+with open("./input.txt", "r", encoding="UTF-8") as fin:
     lines = fin.readlines()
 
 # Part one
@@ -21,9 +21,60 @@ for i, bus in enumerate(busses):
 
 print(minTime * busses[position])
 
-# Part two
+# Part two (Implementation of the Chinese remainder theorem)
 busses = [bus for bus in lines[1].split(",")]
 for i in range(len(busses)):
     if busses[i] != "x":
         busses[i] = int(busses[i])
 
+def moduleInverse(n, m):
+    m0 = m
+    i = 0
+    result = 1
+
+    while(n > 1): 
+        quotient = n // m 
+        temp = m 
+        m = n % m 
+        n = temp
+        temp = i
+        i = result - quotient * i
+        result = temp 
+ 
+    if (result < 0): 
+        result += m0
+    return result
+
+a = []
+n = []
+
+for i, bus in enumerate(busses):
+    if bus != "x":
+        if i == 0:
+            n.append(bus)
+            a.append(0)
+        else:
+            remainder = bus - (i % bus)
+            n.append(bus)
+            a.append(remainder)
+
+product = 1
+for bus in n:
+    product *= bus
+
+prod = []
+for bus in n:
+    prod.append(product // bus)
+
+y = []
+for i in range(len(prod)):
+    y.append(moduleInverse(prod[i], n[i]))
+
+number = 0
+for i in range(len(a)):
+    number += a[i]*prod[i]*y[i]
+
+while (number > product):
+    number -= product
+    
+print(number)
